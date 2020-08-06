4099965b135ce7a08ea91384bdc23bfd8dcc0284
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 10:49:47 -0400

    this a  commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -9,7 +9,7 @@ function Row({ title, fetchUrl }) {
     useEffect(()=> {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
