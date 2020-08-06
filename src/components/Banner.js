2dc673ac51b92c5b40ee1d954c8bb5476a6552a4
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 12:59:40 -0400

    this a commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
+import requests from '../requests';
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            
+            setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+        return requests;
+        }
+        fetchData();
+    }, []);
+    
     return (
         <header>   {/* Background Image */}
             {/* title */}
