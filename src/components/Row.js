5c0e3467f77d2b36402b44737404836462eec671
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 11:49:09 -0400

    this a commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
+import "./Row.css";
 
-const base_url= "https://image.tmdb.org/t/p/original/";
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState ([]);
-    // using react hook {useEffect} to run code on a specific condition
-    // empty[] bracket define that it runs once when row loads, without a reload
-    useEffect(()=> {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results)
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+	const [movies, setMovies] = useState([]);
+	// using react hook {useEffect} to run code on a specific condition
+	// empty[] bracket define that it runs once when row loads, without a reload
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchUrl]);
 
-    console.table(movies)
-    console.log(movies)
-    return (
-        /* title of Row */
-        <div className="row">
-            <h2>{title}</h2>
-        
-    {/* wrapper contains film/tv poster image */}
-            <div className="posters__row">
-                {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-                ))}
-            </div>
-        </div>
-    )
+	console.table(movies);
+	console.log(movies);
+	return (
+		/* title of Row */
+		<div className="row">
+			<h2>{title}</h2>
+
+			{/* wrapper contains film/tv poster image */}
+			<div className="row__posters">
+				{movies.map(movie => (
+					<img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Row;
-;
