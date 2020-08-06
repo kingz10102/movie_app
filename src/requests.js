4290066c2a49c3d41ce21b43e7561632b0d564c6
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-05 22:50:01 -0400

    this a  commit

--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,14 @@ const API_KEY = "9e48e46941b49cb1566bd41c28833612";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated:`/movies/top_rated?api_key=${API_KEY}&language=en-US`
+    fetchTopRated:`/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+
 }
 
 export default requests;
