9c882b9de6caff3b89f137a8788061b8fd560bbd
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 14:57:54 -0400

    this a commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,10 +8,10 @@ function Nav() {
                 src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
                 alt="Netflix logo"
             />
-            {/* <img
-                className='nav-avatar'
+            <img
+                className='nav__avatar'
                 src="https://pbs.twimg.com/media/DN1OYIFX0AAbOMe?format=jpg&name=small"
-                alt='User icon'/> */}
+                alt='User icon'/>
         </div>
     )
 }
