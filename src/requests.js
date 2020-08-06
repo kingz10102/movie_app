6029cac0cb2267508b673679801ce4331f9035db
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-05 22:13:49 -0400

    this a  commit

---
+++ src/requests.js
@@ -0,0 +1,8 @@
+const API_KEY = "9e48e46941b49cb1566bd41c28833612";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language-en-US`,
+    fetchNetflixOriginals:
+}
+
+export default requests;
