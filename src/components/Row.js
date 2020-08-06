e8a38df3835de2dccafac0a29f34a80b4536a704
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 10:47:57 -0400

    this a  commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import axios from './axios'
+import axios from '../axios';
 
 
-function Row({title}, fetchUrl) {
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState ([]);
     // using react hook {useEffect} to run code on a specific condition
     // empty[] bracket define that it runs once when row loads, without a reload
-    useEffect(()=>{
+    useEffect(()=> {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             console.log(request);
             return request;
         }
-        fetchData()
-    }, [])
+        fetchData();
+    }, [fetchUrl]);
     return (
         <div>
             {/* title of Row */}
