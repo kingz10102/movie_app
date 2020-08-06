5df238d980f1abf8383ea826545a66238ca9150f
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 12:43:56 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Row from './components/Row';
 import './App.css';
-import requests from './requests'
+import requests from './requests';
+import Banner from './components/Banner';
 
 
 function App() {
   return (
     <div className="App">
-      
+      {/* Nav */}
+      {/* Banner */}
+      <Banner/>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title= "Trending NOW" fetchUrl={requests.fetchTrending}/>
       <Row title= "Top Rated" fetchUrl={requests.fetchTrending}/>
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <header>   {/* Background Image */}
+            {/* title */}
+            {/* 2 button */}
+            {/* description */}
+        </header>
+    )
+}
+
+export default Banner
