88983a7236e034a861187f049ef8bdd676fa08d1
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 13:47:16 -0400

    this a commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
 import requests from '../requests';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -14,7 +15,12 @@ function Banner() {
         }
         fetchData();
     }, []);
-    console.log(movie)
+    console.log(movie);
+
+    // below truncate function will execuate if exceed number of charters on screen to '...'notation 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
     
     return (
         <header className="banner"
@@ -25,7 +31,7 @@ function Banner() {
         }}>   {/* Background Image */}
             <div className="banner__contents">
                {/* title */}
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                  {/* 2 button */}
