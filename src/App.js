e2b124a241b64cae3484eaa80b55c57f5fa59a8a
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-06 12:35:26 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title= "Trending NOW" fetchUrl={requests.fetchTrending}/>
-      <Row />
+      <Row title= "Top Rated" fetchUrl={requests.fetchTrending}/>
+      <Row title= "Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title= "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title= "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title= "Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title= "Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      
     </div>
   );
 }
