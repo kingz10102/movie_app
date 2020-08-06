863568f20dd6071952967bdc660c1bd7a24c262a
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-05 22:16:49 -0400

    this a  commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Row title=""/>
+      <Row />
+      <Row />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "9e48e46941b49cb1566bd41c28833612";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language-en-US`,
-    fetchNetflixOriginals:
+    fetchNetflixOriginals:``
 }
 
 export default requests;
